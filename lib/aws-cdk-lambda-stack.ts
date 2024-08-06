6a69d6719fd3e14f32f8d130b34f61b36d1701b7
tomohiro_sugiyama <tomo.soul.15@gmail.com>
2024-08-07 00:56:47 +0900

    lambda関数の定義

--- lib/aws-cdk-lambda-stack.ts
+++ lib/aws-cdk-lambda-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda"
 
 export class AwsCdkLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // lambda関数を定義
+    const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
+      code: lambda.DockerImageCode.fromImageAsset("./image"),
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(10),
+      architecture: lambda.Architecture.ARM_64
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // lambda関数にURLを追加してインターネット経由でlambda関数を呼び出す
+    const functionUrl = dockerFunc.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+        allowedOrigins: ["*"],
+      },
+    });
+
+    // CloudFormationに出力値を追加する
+    new cdk.CfnOutput(this, "FunctionUrlValue", {
+      value: functionUrl.url,
+    });
   }
 }
