4e4858ee4f55d0f658f462500f14e47273d19bbb
tomohiro_sugiyama <tomo.soul.15@gmail.com>
2024-08-08 03:11:22 +0900

    コードのリファクタリング

--- lib/aws-cdk-lambda-stack.ts
+++ lib/aws-cdk-lambda-stack.ts
@@ -6,27 +6,44 @@ export class AwsCdkLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // lambda関数を定義
-    const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
+    // lambda関数を作成する
+    const dockerFunc = this.createLambdaFunction();
+    
+    // lambda関数にURLを追加する
+    const functionUrl = this.addFunctionUrl(dockerFunc);
+
+    // CloudFormationに出力値を追加する
+    this.addFunctionUrlOutput(functionUrl);
+  }
+
+  // Lambda関数を作成するメソッド
+  private createLambdaFunction(): lambda.DockerImageFunction {
+    return new lambda.DockerImageFunction(this, "DockerFunc", {
       code: lambda.DockerImageCode.fromImageAsset("./image"),
       memorySize: 1024,
       timeout: cdk.Duration.seconds(10),
       architecture: lambda.Architecture.ARM_64
     });
+  }
 
-    // lambda関数にURLを追加してインターネット経由でlambda関数を呼び出す
-    const functionUrl = dockerFunc.addFunctionUrl({
+  // Lambda関数にURLを追加するメソッド
+  private addFunctionUrl(dockerFunc: lambda.DockerImageFunction): lambda.FunctionUrl {
+    const corsSettings = {
+      allowedMethods: [lambda.HttpMethod.ALL],
+      allowedHeaders: ["*"],
+      allowedOrigins: ["*"],
+    };
+
+    return dockerFunc.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
-      cors: {
-        allowedMethods: [lambda.HttpMethod.ALL],
-        allowedHeaders: ["*"],
-        allowedOrigins: ["*"],
-      },
-    });
-
-    // CloudFormationに出力値を追加する
-    new cdk.CfnOutput(this, "FunctionUrlValue", {
-      value: functionUrl.url,
+      cors: corsSettings
     });
   }
+
+    // CloudFormationに出力値を追加するメソッド
+    private addFunctionUrlOutput(functionUrl: lambda.FunctionUrl): void {
+      new cdk.CfnOutput(this, "FunctionUrlValue", {
+        value: functionUrl.url,
+      });
+    }
 }
